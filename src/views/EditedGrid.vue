cbabf38a8d56a3121171accf61673701d7d03e49
nguyenvantung110 <nguyenvantung11102000@gmail.com>
2024-05-03 12:12:19 +0900

    feat : override data on the table

--- src/views/EditedGrid.vue
+++ src/views/EditedGrid.vue
@@ -11,6 +11,7 @@
         <table @paste.prevent="handlePaste($event)" @click="clickTable($event)" @blur="focusoutTable()" @keydown.ctrl="handleCtrlKeyDown"
             @keyup.ctrl="handleCtrlKeyUp"
             @keydown.ctrl.c="handleCopy"
+
             tabindex="0">
             <thead>
                 <tr>
@@ -23,8 +24,11 @@
                     <td>
                         <input type="checkbox" @change="toggleRowSelection(rowIndex)" :checked="isRowSelected(rowIndex)" />
                     </td>
-                    <td v-for="(value, field,index) in row" :key="field + index" @click="startEditing(rowIndex, field)" :class="{'editing' : isEditing(rowIndex, field)}">
-                        <input v-if="isEditing(rowIndex, field)" v-model="data[rowIndex][field]" @keyup.enter="stopEditing" @keyup.esc="cancelEditing" @blur="stopEditing" autofocus />
+                    <td v-for="(value, field,index) in row" :key="field + index"
+                        @mousedown="handleMouseDown(rowIndex,field.toString(),index)"
+                         @click="startEditing(rowIndex, field.toString(),index)" 
+                         :class="{'editing' : isEditing(rowIndex, field.toString())}">
+                        <input v-if="isEditing(rowIndex, field.toString())" v-model="data[rowIndex][field]" @keyup.enter="stopEditing" @keyup.esc="cancelEditing" @blur="stopEditing" autofocus />
                         <span v-else>{{ value }}</span>
                     </td>
                 </tr>
@@ -72,13 +76,14 @@ const data = ref<any[]>([{
     }
 ])
 
-const headers = ref(['Col1', 'Col2', 'Col3','Col4','Col5','Col-6'])
+const headers = ref(['Col1', 'Col2', 'Col3','Col4','Col5','Col6'])
 
 const sqlCommand = ref('')
 const validateResult = ref('')
 
 const editingRow = ref(-1)
-const editingCol = ref('')
+const editingCol = ref(-1)
+const editingField = ref('')
 const editingValue = ref('')
 const selectedRows = ref<number[]>([])
 
@@ -86,16 +91,24 @@ const isClickTable = ref(false)
 const isCtrlPressed = ref(false)
 const isCopyOnTable = ref(false)
 
-const tableComponentClick = ref<string|undefined>('')
-
 // params for tfoot
 const newRowsToAdd = ref<any[]>([])
 const isEditNewRow = ref(false)
-const currentEditRow = ref(-1)
 
 onMounted(()=>{
   TFootInit()
 })
+
+const isMouseDown = ref(false)
+
+const handleMouseDown = (rowIndex:number,field: string, colIndex : number) => {
+    if(rowIndex !== editingRow.value && colIndex !== editingCol.value)
+    {
+        stopEditing()
+    }
+
+    isMouseDown.value = true
+}
 
 function TFootInit(){
   const newItem :any = {}
@@ -109,52 +122,67 @@ function TFootInit(){
 function clickTable(event : MouseEvent) {
   isClickTable.value = true
 
-  const target = event.target as HTMLElement;
-
-  const clickedElement = target.closest('thead') || target.closest('tbody') || target.closest('tfoot');
+  const clickedElement = TableElementClick(event);
   if(isEditNewRow.value && clickedElement?.tagName.toLowerCase() !== 'tfoot'){
     AddNewRowToDataList()
   }
 }
 
+function TableElementClick(event : MouseEvent){
+    const target = event.target as HTMLElement;
+
+    const clickedElement = target.closest('thead') || target.closest('tbody') || target.closest('tfoot');
+
+    return clickedElement
+}
+
+// user focus into table
 function focusoutTable() {
-    isClickTable.value = false
-    isCopyOnTable.value = false
+    if (!isMouseDown.value) {
+        isClickTable.value = false
+        isCopyOnTable.value = false
+        stopEditing()
+    } 
 }
 
+// ctrl key is pressed down
 function handleCtrlKeyDown() {
-      isCtrlPressed.value = true;
-    }
-  
-function handleCtrlKeyUp() {
-      isCtrlPressed.value = false;
-    }
-
-function handleCopy() {
-      if (isCtrlPressed.value) {
-        isCopyOnTable.value = true
-      }
-    }
-
-
-function startEditing(row: number, col: string) {
-    editingRow.value = row;
-    editingCol.value = col;
-    editingValue.value = data.value[row][col];
+    isCtrlPressed.value = true;
 }
 
+// ctrl key is released
+function handleCtrlKeyUp() {
+    isCtrlPressed.value = false;
+}
+
+// User use key combination Ctrl + C on the table
+function handleCopy() {
+    isCopyOnTable.value = isCtrlPressed.value
+}
+
+// User double click into cell
+function startEditing(row: number, field: string, column : number) {
+    editingRow.value = row;
+    editingField.value = field;
+    editingCol.value = column
+    editingValue.value = data.value[row][field];
+}
+
+// User press Enter key or focusout cell
 function stopEditing() {
     editingRow.value = -1;
-    editingCol.value = '';
+    editingField.value = ''
+    editingCol.value = -1
 }
 
+// User press Escape key
 function cancelEditing() {
-    data.value[editingRow.value][editingCol.value] = editingValue.value;
+    data.value[editingRow.value][editingField.value] = editingValue.value;
     stopEditing();
 }
 
-function isEditing(row: number, col: string) {
-    return editingRow.value === row && editingCol.value === col;
+function isEditing(row: number, field: string) {
+    return editingRow.value === row && editingField.value === field;
 }
 
 function isRowSelected(rowIndex: number) {
@@ -187,6 +215,7 @@ function ValidateCommand() {
 }
 
 function handlePaste(event: any) {
+    console.log('row - ' + editingRow.value + '\r\ncolumn - ' + editingCol.value)
     const clipboardData = event.clipboardData || window.Clipboard;
     const pastedData = clipboardData.getData('text/plain');
 
@@ -197,6 +226,8 @@ function handlePaste(event: any) {
 }
 
 function processData(inputdata: any) {
+    // console.log('inputdata ',inputdata.split(/\r?\n\t?|\t\r?\n?/))
+    // console.log('parseData ',JSON.stringify(inputdata))
     const parseData = inputdata.split('\r\n').map((row: any) => {
       if(isCopyOnTable.value)
       {
@@ -205,33 +236,80 @@ function processData(inputdata: any) {
       return row.split('\t')
     });
 
+
     const rows = parseData.filter((row: any) => !row.every((element: any) => !IsNotNullOrEmpty(element)));
 
     if (rows.length > 0) {
-        rows.forEach((row: any) => {
-            const newRow: any = {};
-            for (const [index, value] of headers.value.entries()) {
-                newRow[index] = IsNotNullOrEmpty(row[index]) ? row[index] : ''
+        const intervalIndex = data.value.length - editingRow.value
+        const endLoop = intervalIndex > rows.length ? rows.length : intervalIndex
+
+        if(editingRow.value === -1 && editingCol.value === -1)
+        {
+            rows.forEach((row: any) => {
+                const newRow: any = {};
+
+                for (const [index, value] of headers.value.entries()) {
+                    newRow[index] = IsNotNullOrEmpty(row[index]) ? row[index] : ''
+                }
+
+                data.value.push(newRow)
+            })
+        }
+        else 
+        {
+            for(let i = editingRow.value; i < editingRow.value + endLoop; i ++){
+                const intervalColumn =  Object.keys(data.value[i]).length - editingCol.value
+                const columnCount = rows[i-editingRow.value].length > intervalColumn ?intervalColumn : rows[i-editingRow.value].length
+
+                for(let j = editingCol.value ; j < columnCount + editingCol.value; j++)
+                {
+                    const key  = Object.keys(data.value[i])[j]
+                    data.value[i][key] = rows[i-editingRow.value][j - editingCol.value]
+                }
             }
 
-            data.value.push(newRow)
-        })
+            if(intervalIndex < rows.length)
+            {
+                for(let i = intervalIndex ; i < rows.length; i ++)
+                {
+                    const newRow: any = {};
+
+                    for (let j = editingCol.value; j < headers.value.length ; j++) {
+                        newRow[j] = IsNotNullOrEmpty(rows[i][j-editingCol.value]) ? rows[i][j-editingCol.value] : ''
+                    }
+
+                    for (let j = 0; j < editingCol.value ; j++) {
+                        newRow[j] = ''
+                    }
+
+                    data.value.push(newRow)
+                }
+            }
+        }
+        
     }
 }
 
+// List of function key on the keyboard
 const nonPrintableKeys = [
     'Shift', 'Control', 'Alt','NumLock','Meta', 'CapsLock', 'Escape', 'Tab', 'Enter', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown',
     'Home', 'End', 'PageUp', 'PageDown', 'Insert', 'Delete', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
   ];
 
 function AddNewEmptyRow(event:KeyboardEvent,rowIndex : number,index : number){
-  if(newRowsToAdd.value.length <2 && !nonPrintableKeys.includes(event.key))
+  if(newRowsToAdd.value.length <2 && !nonPrintableKeys.includes(event.key) && !IsPaseData(event))
   {
     TFootInit()
   }
 } 
 
+function IsPaseData(event : KeyboardEvent){
+    return event.ctrlKey && event.key === 'v'
+}
+
 function EditNewRow(index : number){
+  stopEditing()
+
   if(index > 0){
     AddNewRowToDataList()
   }
@@ -242,12 +320,16 @@ function FocusoutNewRow(index:number){
 }
 
 function AddNewRowToDataList(){
-    data.value.push(newRowsToAdd.value[0])
-    newRowsToAdd.value.shift()
-    isEditNewRow.value = false
+    const hasNonNullValues = Object.values(newRowsToAdd.value[0]).some(value => IsNotNullOrEmpty(value));
+    if(hasNonNullValues)
+    {
+        data.value.push(newRowsToAdd.value[0])
+        newRowsToAdd.value.shift()
+        isEditNewRow.value = false
+    }
 }
 
-function IsNotNullOrEmpty(value: string) {
+function IsNotNullOrEmpty(value: any) {
     return value !== null && value !== '' && value !== undefined
 }
 </script>
